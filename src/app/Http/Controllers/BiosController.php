c0a1208800b7e51d0463fa4a33df00848e5c3ad9
Michel Bolzon <michel.bolzon123@gmail.com>
2025-10-25 17:52:22 -0300

    Add bios translate for michelbolzon project

---
+++ src/app/Http/Controllers/BiosController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BiosController extends Controller
+{
+    public function index(){
+        return view('bios');
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BiosController;
 use App\Http\Controllers\CurriculumController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CurriculumController::class, 'curriculum'])->name('curriculum');
 Route::post('/contact', [CurriculumController::class, 'contact'])->name('contact');
+Route::get('/bios', [BiosController::class, 'index'])->name('bios.index');
